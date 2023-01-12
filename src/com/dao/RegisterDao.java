88d429dd0d42e7081275cd13100f1d2ca69f210f
iamananth <ananthrshenoy@gmail.com>
2023-01-12 13:25:32 +0530

    user register using hibernate

--- src/com/dao/RegisterDao.java
+++ src/com/dao/RegisterDao.java
@@ -5,32 +5,61 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.security.MessageDigest;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.pojo.User;
+
 public class RegisterDao {
-	public boolean insert(String uname, String pass, String dept, String ph){
-		Connection con = null;
-        PreparedStatement pstmt = null;
-        boolean status = false;
-        
-        try{
-        	 MessageDigest md = MessageDigest.getInstance("MD5");
-             byte[] hash = md.digest(pass.getBytes());
-             String hashedPassword = new String(hash, "UTF-8");
-        	
-        	Class.forName("oracle.jdbc.driver.OracleDriver");
-        	con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","hr","root");
-        	pstmt = con.prepareStatement("INSERT INTO users(username, password, department, phone_number) VALUES (?, ?, ?, ?)");
-        	
-        	pstmt.setString(1, uname);
-        	pstmt.setString(2, hashedPassword);
-        	pstmt.setString(3, dept);
-        	pstmt.setString(4, ph);
-        	pstmt.executeUpdate();
-        	
-        	status = true;
-        	
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-		return status;
-	}
+//	public boolean insert(String uname, String pass, String dept, String ph){
+//		Connection con = null;
+//        PreparedStatement pstmt = null;
+//        boolean status = false;
+//        
+//        try{
+//        	 MessageDigest md = MessageDigest.getInstance("MD5");
+//             byte[] hash = md.digest(pass.getBytes());
+//             String hashedPassword = new String(hash, "UTF-8");
+//        	
+//        	Class.forName("oracle.jdbc.driver.OracleDriver");
+//        	con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","hr","root");
+//        	pstmt = con.prepareStatement("INSERT INTO users(username, password, department, phone_number) VALUES (?, ?, ?, ?)");
+//        	
+//        	pstmt.setString(1, uname);
+//        	pstmt.setString(2, hashedPassword);
+//        	pstmt.setString(3, dept);
+//        	pstmt.setString(4, ph);
+//        	pstmt.executeUpdate();
+//        	
+//        	status = true;
+//        	
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//		return status;
+//	}
+	
+	public int register(User u){    
+		 int i=0;    
+		  
+		 StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();  
+		 Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();  
+		  
+		SessionFactory factory = meta.getSessionFactoryBuilder().build();  
+		Session session = factory.openSession();  
+		Transaction t = session.beginTransaction();   
+		  
+		i=(Integer)session.save(u);    
+		  
+		t.commit();    
+		session.close();    
+		    
+		return i;    
+		   
+		 }    
 }
--- src/com/servlet/UserRegister.java
+++ src/com/servlet/UserRegister.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.dao.RegisterDao;
+import com.pojo.User;
 
 /**
  * Servlet implementation class UserRegister
@@ -23,10 +24,22 @@ public class UserRegister extends HttpServlet {
 		String ph = request.getParameter("ph");
 		String pass = request.getParameter("password");
 		
-		RegisterDao dao = new RegisterDao();
-		boolean st = dao.insert(name, pass, dept, ph);
+		User u = new User();
+		u.setUsername(name);
+		u.setDepartment(dept);
+		u.setPhone_number(ph);
+		u.setPassword(pass);
 		
-		if(st){
+		RegisterDao dao = new RegisterDao();
+//		boolean st = dao.insert(name, pass, dept, ph);
+		
+		int i = dao.register(u);
+		
+//		if(st){
+//			response.sendRedirect("UserLogin.jsp");
+//		}
+		
+		if(i > 0){
 			response.sendRedirect("UserLogin.jsp");
 		}
 	}
